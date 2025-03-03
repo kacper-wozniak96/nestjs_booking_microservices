e2211ddeadb98dc7ecb3e95676161e69bc5d83f4
str <kacper.wozniak966@gmail.com>
2025-03-03 19:21:55 +0100

    Added common auth guard

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@app/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UserDocument, UserSchema } from './models/user.schema';
 import { UsersRepository } from './users.repository';
+import { UserDocument, UserSchema } from './models/user.schema';
 
 @Module({
   imports: [
@@ -14,5 +14,6 @@ import { UsersRepository } from './users.repository';
   ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,6 +1,7 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 import { AbstractDocument } from './abstract.schema';
+import { CreateIndexesOptions } from 'mongodb';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   protected abstract readonly logger: Logger;
@@ -16,11 +17,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   }
 
   async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
-    const document = (await this.model.findOne(
-      filterQuery,
-      {},
-      { lean: true },
-    )) as unknown as TDocument;
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
 
     if (!document) {
       this.logger.warn('Document not found with filterQuery', filterQuery);
@@ -33,7 +30,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   async findOneAndUpdate(
     filterQuery: FilterQuery<TDocument>,
     update: UpdateQuery<TDocument>,
-  ): Promise<TDocument> {
+  ) {
     const document = await this.model.findOneAndUpdate(filterQuery, update, {
       lean: true,
       new: true,
@@ -44,22 +41,18 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
       throw new NotFoundException('Document not found.');
     }
 
-    return document as unknown as TDocument;
+    return document;
   }
 
-  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
-    return (await this.model.find(
-      filterQuery,
-      {},
-      { lean: true },
-    )) as unknown as TDocument[];
+  async find(filterQuery: FilterQuery<TDocument>) {
+    return this.model.find(filterQuery, {}, { lean: true });
   }
 
-  async findOneAndDelete(
-    filterQuery: FilterQuery<TDocument>,
-  ): Promise<TDocument> {
-    return this.model.findOneAndDelete(filterQuery, {
-      lean: true,
-    }) as unknown as TDocument;
+  async findOneAndDelete(filterQuery: FilterQuery<TDocument>) {
+    return this.model.findOneAndDelete(filterQuery, { lean: true });
+  }
+
+  async createIndex(options: CreateIndexesOptions) {
+    return this.model.createIndexes(options as any);
   }
 }
